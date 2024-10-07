369ba6f6e89e069439b6e64082179550bd23754b
Talentn <alaghabi98@gmail.com>
2024-10-07 10:49:12 +0100

    testing again

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import Rails from "@rails/ujs"
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap';
+
 Rails.start()
